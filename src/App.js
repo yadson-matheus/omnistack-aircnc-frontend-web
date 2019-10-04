48cbb6e032ff21f2a974e22c117e7c839ca7cda7
yadson18 <yadson18@hotmail.com>
2019-10-04 01:50:10 -0300

    Adding project to repository

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import api from './services/api';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [email, setEmail] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        
+        console.log('OK');
+    }
+
+    return (
+        <div className="container">
+            <img src={ logo } alt="Logo AirCnC"/>
+            <div className="content">
+                <p>
+                    Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
+                </p>
+
+                <form onSubmit={ handleSubmit }>
+                    <label htmlFor="email">E-MAIL *</label>
+                    <input 
+                        id="email" 
+                        type="email" 
+                        placeholder="Seu melhor e-mail" 
+                        value={ email }
+                        onChange={ event => setEmail(event.target.value) }
+                    />
+                    <button type="submit" className="btn">Entrar</button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default App;
