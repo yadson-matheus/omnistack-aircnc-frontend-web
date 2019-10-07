7bad64f09ee366424385ac76da4524162b5ae624
yadson18 <yadson18@hotmail.com>
2019-10-06 22:21:51 -0300

    Finish application (consuming API REST)

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
-import React, { useState } from 'react';
-import api from './services/api';
+import React from 'react';
 import './App.css';
+
+import Routes from './routes';
 
 import logo from './assets/logo.svg';
 
 function App() {
-    const [email, setEmail] = useState('');
-
-    function handleSubmit(event) {
-        event.preventDefault();
-        
-        console.log('OK');
-    }
-
     return (
         <div className="container">
             <img src={ logo } alt="Logo AirCnC"/>
             <div className="content">
-                <p>
-                    Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
-                </p>
-
-                <form onSubmit={ handleSubmit }>
-                    <label htmlFor="email">E-MAIL *</label>
-                    <input 
-                        id="email" 
-                        type="email" 
-                        placeholder="Seu melhor e-mail" 
-                        value={ email }
-                        onChange={ event => setEmail(event.target.value) }
-                    />
-                    <button type="submit" className="btn">Entrar</button>
-                </form>
+                <Routes />
             </div>
         </div>
     );
